c8b10617a10dad3933404d76135f8cd878592157
rahul-sirpi <rahul@sirpi.io>
2025-05-04 14:20:31 +0530

    /  path change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
