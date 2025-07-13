45e387ad11cb51905eae165ec1efc325b229570c
rahul-kapgate <rahulkapgateyt@gmail.com>
2025-07-13 17:47:48 +0530

    Implement Course Navigation and Mock Test Features: Introduce MainCoursePage, MockTestPage, and VideoLecturesPage components. Update routing in App.tsx to support new course navigation structure, enhancing user experience with dedicated pages for resources, mock tests, and video lectures.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./layouts/Layout";
 import Courses from "./pages/Courses";
-import ResourcePage from "./pages/CoursePage/ResourcePage.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import AdminHome from "./pages/AdminPanel/AdminHome.tsx";
 import "./App.css"; // Import custom CSS for toast styles
+import MainCoursePage from "./pages/CoursePage/MainCoursePage.tsx";
+import ResourcePage from "./pages/CoursePage/ResourcePage.tsx";
+import MockTestPage from "./pages/CoursePage/MockTestPage.tsx";
+import VideoLecturesPage from "./pages/CoursePage/VideoLecturesPage.tsx";
 
 function App() {
   return (
@@ -30,7 +33,19 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="/courses" element={<Courses />} />
-            <Route path="/course/:courseName" element={<ResourcePage />} />
+            <Route path="/course/:courseName" element={<MainCoursePage />} />
+            <Route
+              path="/course/:courseName/resources"
+              element={<ResourcePage />}
+            />
+            <Route
+              path="/course/:courseName/mock-test"
+              element={<MockTestPage />}
+            />
+            <Route
+              path="/course/:courseName/video-lectures"
+              element={<VideoLecturesPage />}
+            />
             <Route path="/admin" element={<AdminHome />} />
           </Route>
         </Routes>
